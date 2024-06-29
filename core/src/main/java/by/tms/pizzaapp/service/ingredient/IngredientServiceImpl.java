b5ca0720829615299cf774a195e2b4086419660e
ykondratenko87 <y.kondratenko87@gmail.com>
2024-06-29 17:12:08 +0300

    add some changes14

--- core/src/main/java/by/tms/pizzaapp/service/ingredient/IngredientServiceImpl.java
+++ core/src/main/java/by/tms/pizzaapp/service/ingredient/IngredientServiceImpl.java
@@ -5,6 +5,9 @@ import by.tms.pizzaapp.entity.ingredient.Ingredient;
 import by.tms.pizzaapp.mapper.IngredientMapper;
 import by.tms.pizzaapp.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +22,7 @@ public class IngredientServiceImpl implements IngredientService {
     private final IngredientMapper ingredientMapper;
 
     @Override
+    @Cacheable(value = "ingredients")
     public List<IngredientResponse> getAllIngredients() {
         return ingredientRepository.findAll().stream()
                 .map(ingredientMapper::toResponse)
@@ -26,6 +30,7 @@ public class IngredientServiceImpl implements IngredientService {
     }
 
     @Override
+    @CacheEvict(value = "ingredients", allEntries = true)
     public IngredientResponse createIngredient(IngredientRequest ingredientRequest) {
         Optional<Ingredient> existingIngredientOpt = ingredientRepository
                 .findByNameAndPrice(ingredientRequest.getName(), ingredientRequest.getPrice());
@@ -41,11 +46,13 @@ public class IngredientServiceImpl implements IngredientService {
     }
 
     @Override
+    @CacheEvict(value = "ingredients", key = "#id")
     public void deleteIngredientById(Long id) {
         ingredientRepository.deleteById(id);
     }
 
     @Override
+    @CachePut(value = "ingredients", key = "#id")
     public IngredientResponse updateIngredientById(Long id, IngredientRequest ingredientRequest) {
         Optional<Ingredient> existingIngredientOpt = ingredientRepository.findById(id);
         if (existingIngredientOpt.isPresent()) {
--- core/src/main/java/by/tms/pizzaapp/service/review/ReviewServiceImpl.java
+++ core/src/main/java/by/tms/pizzaapp/service/review/ReviewServiceImpl.java
@@ -6,6 +6,9 @@ import by.tms.pizzaapp.entity.review.Review;
 import by.tms.pizzaapp.mapper.ReviewMapper;
 import by.tms.pizzaapp.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,16 +25,19 @@ public class ReviewServiceImpl implements ReviewService {
     private final PizzaRepository pizzaRepository;
 
     @Override
+    @Cacheable(value = "reviews")
     public List<ReviewResponse> getAllReviews() {
         return reviewRepository.findAll().stream().map(reviewMapper::toResponse).collect(Collectors.toList());
     }
 
     @Override
+    @Cacheable(value = "reviews", key = "#id")
     public Optional<ReviewResponse> getReviewById(long id) {
         return reviewRepository.findById(id).map(reviewMapper::toResponse);
     }
 
     @Override
+    @CacheEvict(value = "reviews", allEntries = true)
     public ReviewResponse createReview(ReviewRequest reviewRequest) {
         Pizza pizza = pizzaRepository.findById(reviewRequest.getPizzaId())
                 .orElseThrow(() -> new IllegalArgumentException("Pizza with ID " + reviewRequest.getPizzaId() + " not found"));
@@ -42,6 +48,7 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
+    @CacheEvict(value = "reviews", key = "#id")
     public boolean deleteReviewById(long id) {
         if (reviewRepository.existsById(id)) {
             reviewRepository.deleteById(id);
@@ -52,6 +59,7 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
+    @CachePut(value = "reviews", key = "#id")
     public Optional<ReviewResponse> updateReview(long id, ReviewRequest reviewRequest) {
         Pizza pizza = pizzaRepository.findById(reviewRequest.getPizzaId())
                 .orElseThrow(() -> new IllegalArgumentException("Pizza with ID " + reviewRequest.getPizzaId() + " not found"));
@@ -63,6 +71,7 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
+    @Cacheable(value = "reviews", key = "#pizzaName")
     public List<ReviewResponse> getReviewByPizzaName(String pizzaName) {
         Optional<Pizza> pizza = pizzaRepository.findByName(pizzaName);
         if (pizza.isPresent()) {
@@ -75,6 +84,7 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
+    @Cacheable(value = "reviews")
     public Page<ReviewResponse> getAllReviewsWithPagination(Pageable pageable) {
         return reviewRepository.findAll(pageable).map(reviewMapper::toResponse);
     }
